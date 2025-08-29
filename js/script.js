5e02c4f310033b9d2f55111b99228ffa17e55ffb
camilariostorres <camilariost24@gmail.com>
2025-08-29 11:47:53 -0300

    Create script.js

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const DATA_URL = "json/data.json";
+const container = document.getElementById("container");
+
+function showData(dataArray) {
+  for (const item of dataArray) {
+    container.innerHTML += `<p>${item.name} ${item.lastname}</p>`;
+  }
+}
+
+fetch(DATA_URL)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("Error al cargar el archivo JSON");
+    }
+    return response.json();
+  })
+  .then(data => {
+    showData(data.students); // usamos el array de estudiantes
+  })
+  .catch(error => {
+    console.error("Hubo un problema con el fetch:", error);
+  });
